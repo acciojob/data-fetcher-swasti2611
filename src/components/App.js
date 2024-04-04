af86f764f1ab7d0d58e1395f85796ce848bbc04d
Swati jagtap <swatijagtap5060@gmail.com>
2024-04-04 13:18:17 +0530

    added

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,36 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState, useEffect } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [data, setData] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/products")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(JSON.stringify(data));
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setIsLoading(false);
+      });
+  }, []); // Empty dependency array ensures that this effect runs only once
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {isLoading ? (
+        <p>Loading....</p>
+      ) : (
+        <div>
+          <h1>DATA FETCHED FROM API</h1>
+          <pre>{data}</pre>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
